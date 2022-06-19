9ad0b52d01dfaa4818db7bc7961ea7ad53cceca1
true spirit <truespirit7777@gmail.com>
2022-06-19 16:12:02 +0700

    chat

--- app/Http/Controllers/Home/Chatbot/ChatbotController.php
+++ app/Http/Controllers/Home/Chatbot/ChatbotController.php
@@ -31,11 +31,12 @@ class ChatbotController extends Controller
         $categories = Category::orderBy('created_at', 'DESC')->get();
         $postTags = PostTag::orderBy('created_at', 'DESC')->get();
         $tags = Tag::orderBy('created_at', 'DESC')->get();
-
+        $messages = Message::latest()->limit(5)->get();
 
         return view('home.chatbot', ['categories' => $categories,
                 'postTags' => $postTags,
                 'tags' => $tags,
+            'messages'=>$messages
             ]
         );
 
@@ -55,32 +56,8 @@ class ChatbotController extends Controller
             'message' => $request->input('message')
         ]);
         broadcast(new MessageSent($user, $message))->toOthers();
-                $data = (object) [
-            "context" => $request->input('message'),
-//            "context" => $request->input('message'),
-            "length"=> 30,
-            "k"=> 0,
-            "p"=> 0.9,
-            "temperature"=> 1.0 ,
-            "rp"=> 1.0,
-            "nrs"=> 1,
-            "seed"=> 42
-        ];
-//
-//        $data = (object) [
-//            "context" => $request->input('message'),
-//        'max_length' => 256,
-//        'no_repeat_ngram_size'=> 1,
-//        'do_sample'=> True,
-//        'top_k'=> 100,
-//        'top_p'=> 0.9,
-//        'temperature'=> 0.6,
-//        'num_return_sequences'=> 5,
-//        'device'=> 0,
-//        'is_always_use_length'=> True,
-//        'length_generate'=> '1'
-//    ];
-        $url = 'http://10.8.0.5:8000/';
+
+        $url = 'http://10.8.0.5:8020/dialog';
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
@@ -91,16 +68,55 @@ class ChatbotController extends Controller
             'Content-Type: application/json',
         );
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
 
-        $response = curl_exec ($curl);
-        $err = curl_error($curl);  //if you need
-        curl_close ($curl);
+        $messages = Message::latest()->limit(15)->get()->toArray();
+
+        usort($messages, function($a, $b)
+        {
+            return strcmp($a['id'], $b['id']);
+        });
+
+        $messages = array_map(function ($message) {
+            return (object) [
+                'speaker' => $message['user_id'] == 2 ? 1 : 0,
+                'text' => $message['message'],
+            ];
+        }, $messages);
+
+        array_push($messages, (object) [
+            'speaker' => 0,
+            'text' => $request->input('message'),
+        ]);
+
+        $data = '{
+            "dialog_history_array": ' . json_encode($messages) . ',
+            "params": {
+                "length": 1,
+                "max_length": 256,
+                "no_repeat_ngram_size": 3,
+                "top_k": 75,
+                "top_p": 0.9,
+                "temperature": 0.6,
+                "num_return": 3,
+                "do_sample": true,
+                "use_gpu": false
+            }
+        }';
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        $result = curl_exec($curl);
+
+        file_put_contents("C:\\laragon\\www\\edu\\app\\Http\\Controllers\\Home\\Chatbot\\umitest", print_r([__FILE__.' '.__LINE__, $data], true).PHP_EOL, FILE_APPEND | LOCK_EX);
+
+        curl_close($curl);
+
+        $obj = json_decode($result);
 
         $socrat = User::where('name', '=', 'Сократ')->get();
         $user = $socrat[0];
         $message = $user->messages()->create([
-            'message' => $response
+            'message' => $obj->response,//outputs[0],
         ]);
         broadcast(new MessageSent($user, $message))->toOthers();
 
