daa74b8de4e4e2532e69815ca84cafa06a74a9b2
true spirit <truespirit7777@gmail.com>
2022-06-20 04:25:57 +0700

    add

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -174,10 +174,10 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-{{--    <a href="index3.html" class="brand-link">--}}
-{{--      <img src="/img/core-img/logo.png" alt="adminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">--}}
-{{--      <span class="brand-text font-weight-light">Antiqua</span>--}}
-{{--    </a>--}}
+    <a href="index3.html" class="brand-link">
+      <img src="/img/core-img/logo.png" alt="adminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Antiqua</span>
+    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
